b9c5eb4adb36624e31ebfc726705d701a7a0496d
todimuisewon <todimuisewon@gmail.com>
2023-07-17 00:49:43 +0100

    project initialization

---
+++ src/main/java/io/todimu/practice/ussdwallet/UssdWalletApplication.java
@@ -0,0 +1,13 @@
+package io.todimu.practice.ussdwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UssdWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UssdWalletApplication.class, args);
+    }
+
+}
