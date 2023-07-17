b89c0411bbba173eab7b2cd5795853211351a6c7
todimuisewon <todimuisewon@gmail.com>
2023-07-17 03:02:59 +0100

    enabled db auditing

---
+++ src/main/java/io/todimu/practice/ussdwallet/config/DatabaseAuditConfig.java
@@ -0,0 +1,21 @@
+package io.todimu.practice.ussdwallet.config;
+
+import io.todimu.practice.ussdwallet.security.SecurityAuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("io.todimu.practice.ussdwallet.repository")
+@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAwareImpl")
+public class DatabaseAuditConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new SecurityAuditorAwareImpl();
+    }
+}
---
+++ src/main/java/io/todimu/practice/ussdwallet/security/SecurityAuditorAwareImpl.java
@@ -0,0 +1,25 @@
+package io.todimu.practice.ussdwallet.security;
+
+import io.todimu.practice.ussdwallet.utils.ResponseConstants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityAuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Optional<String> loggedInUser = getLoggedInUser();
+        return Optional.of(loggedInUser.orElse(ResponseConstants.SYSTEM));
+    }
+
+    private Optional<String> getLoggedInUser() {
+        String principal = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return Optional.of(principal);
+    }
+}
