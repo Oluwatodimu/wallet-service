b42628e20d719da57ca34d28d06dd2798ddd5b18
todimuisewon <todimuisewon@gmail.com>
2023-07-17 01:29:14 +0100

    updated user model

--- src/main/java/io/todimu/practice/ussdwallet/model/User.java
+++ src/main/java/io/todimu/practice/ussdwallet/model/User.java
@@ -6,26 +6,43 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import javax.validation.constraints.Pattern;
-import java.util.UUID;
+import java.time.Instant;
 
 @Data
 @Entity
 @Table(name = "ussd_user")
 public class User extends BaseEntity {
 
-    @Column(name = "user_id", updatable = false)
-    private UUID userId;
-
     @JsonIgnore
     @Column(name = "password_hash")
     private String password;
+
+    @JsonIgnore
+    @Column(name = "ussd_pin_hash")
+    private String ussdPin;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", updatable = false)
+    @Pattern(regexp = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
+    private String email;
 
     @Column(name = "phone_number")
     @Pattern(regexp = "^\\d{13}$")
     private String phoneNumber;
 
+    @Column(name = "image_url")
+    private String imageUrl;
+
     @Column(name = "activated")
-    private Boolean activated = true;
+    private boolean activated = false;
+
+    @Column(name = "password_reset_date")
+    private Instant passwordResetDate = Instant.now();
 
     @Enumerated(EnumType.STRING)
     @Column(name = "user_status")
