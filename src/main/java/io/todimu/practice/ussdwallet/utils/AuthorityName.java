1996331cc7c0f1049f473ffd95dad77240bf3e01
todimuisewon <todimuisewon@gmail.com>
2023-07-17 03:01:52 +0100

    added utils

---
+++ src/main/java/io/todimu/practice/ussdwallet/utils/AuthorityName.java
@@ -0,0 +1,7 @@
+package io.todimu.practice.ussdwallet.utils;
+
+public class AuthorityName {
+
+    public static final String ADMIN = "ROLE_ADMIN";
+    public static final String USER = "ROLE_USER";
+}
